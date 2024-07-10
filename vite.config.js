22ab58bb7d6c9ac9d30036741ed9bfca5b6fc4e6
yoguimon <jhoninformatic4@gmail.com>
2024-07-10 09:34:13 -0400

    listo para deploy

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: "https://yoguimon.github.io/sistema-ventas-deploy",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
